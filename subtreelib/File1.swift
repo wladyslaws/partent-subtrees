81c1a97ca7320fbebb555040e7f27035fbf6ccaf
Wladyslaw Surala <wladyslaw.surala@moonpig.com>
2017-07-26 13:28:41 +0100

    adding project filez

---
+++ subtreelib/File1.swift
@@ -0,0 +1,15 @@
+//
+//  File1.swift
+//  subtreelib
+//
+//  Created by Wladyslaw Surala on 26/07/2017.
+//  Copyright © 2017 Wladyslaw Surala. All rights reserved.
+//
+
+import Foundation
+
+class subtreelibClass1 {
+    public static func printSomething(){
+        print("something")
+    }
+}
---
+++ subtreelib/subtreelib.h
@@ -0,0 +1,19 @@
+//
+//  subtreelib.h
+//  subtreelib
+//
+//  Created by Wladyslaw Surala on 26/07/2017.
+//  Copyright © 2017 Wladyslaw Surala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for subtreelib.
+FOUNDATION_EXPORT double subtreelibVersionNumber;
+
+//! Project version string for subtreelib.
+FOUNDATION_EXPORT const unsigned char subtreelibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <subtreelib/PublicHeader.h>
+
+
