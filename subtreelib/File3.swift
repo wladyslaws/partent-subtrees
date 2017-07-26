712eb26e07137f17e5e1c7c02418657bde07551f
Wladyslaw Surala <wladyslaw.surala@moonpig.com>
2017-07-26 14:31:48 +0100

    Squashed 'checkout/' changes from 23f6816..2f85e3d
    2f85e3d adding stuff before rebasing
    480c0e1 Merge pull request #3 from wladyslaws/adding-stuff3
    199726d Merge pull request #2 from wladyslaws/adding-stuff2
    7823659 adding serious changes
    b8f106f changes to both subtree and parent 2
    
    git-subtree-dir: checkout
    git-subtree-split: 2f85e3d79a4ba850d1d7fc87b6a9c25dd3fc79cf

---
+++ subtreelib/File3.swift
@@ -0,0 +1,23 @@
+//
+//  File3.swift
+//  subtreelib
+//
+//  Created by Wladyslaw Surala on 26/07/2017.
+//  Copyright © 2017 Wladyslaw Surala. All rights reserved.
+//
+
+import Foundation
+
+public class subtreelibClass3 {
+    public init(){
+    }
+    public func printStuff() {
+        print("stuff")
+    }
+    public func printOtherStuff(){
+        print("other stuff")
+    }
+    public func newStuffez(){
+        print("yay, let's rebase!")
+    }
+}
