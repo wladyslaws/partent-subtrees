bdef53a19f7f8a26da89ede826f703a870384626
Wladyslaw Surala <wladyslaw.surala@moonpig.com>
2017-07-26 13:31:12 +0100

    adding file 2

---
+++ subtreelib/File2.swift
@@ -0,0 +1,15 @@
+//
+//  File2.swift
+//  subtreelib
+//
+//  Created by Wladyslaw Surala on 26/07/2017.
+//  Copyright © 2017 Wladyslaw Surala. All rights reserved.
+//
+
+import Foundation
+
+class subtreelibClass2 {
+    public static func printSomethingElse() {
+        print("something else")
+    }
+}
